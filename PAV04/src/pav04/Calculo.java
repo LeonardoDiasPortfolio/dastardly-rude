1a60625a612cff1b7dd4c9f4a5da21827431ad7a
Leonardo Dias Rezende Junior <leodiasrezende@gmail.com>
2022-08-31 20:50:34 -0300

    Add files via upload
    Calculator!

---
+++ PAV04/src/pav04/Calculo.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pav04;
+
+/**
+ *
+ * @author alunocpti
+ */
+public class Calculo {
+    
+    public int sum(int x, int y){
+       return x + y;       
+    }
+    public int sub(int x, int y){
+       return x - y;       
+    }
+    public int mult(int x, int y){
+       return x * y;       
+    }
+    public int div(int x, int y){
+       return x / y;       
+    }
+}
